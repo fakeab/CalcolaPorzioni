bd388fa24f7fb32105261920d954d99ef6f044e9
fakeab <119085792+fakeab@users.noreply.github.com>
2022-11-26 14:28:00 +0100

    Add files via upload
    - numbers are now rounded (without decimals)
    - added output for both persons

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ var elements;
 var LAST_ONE_EMPTY = 'empty';
 var lastOne = LAST_ONE_EMPTY;
 var somma;
-var risultato;
+var risultato1;
+var risultato2;
 
-calculate = function (a1, a2, b1, b2) {
+
+calculate = function (a1, a2, b1, b2, b3) {
     var valued = '';
+    
 
     if (a1 != '') { valued += '1'; }
     if (a2 != '') { valued += '2'; }
@@ -21,16 +24,18 @@ calculate = function (a1, a2, b1, b2) {
         switch (valued) {
             case '123':
                 somma = +a1 + +a2;
-                risultato = (a1 * b1) / (somma)
-                elements.B2.value = +risultato.toFixed(1);
+                risultato1 = (a1 * b1) / (somma);
+                risultato2 = (a2 * b1) / (somma);
+                elements.B2.value = +risultato1.toFixed(0);
+                elements.B3.value = +risultato2.toFixed(0);
                
                 break;
-            case '124':
-                elements.B1.value = b2 * a1 / a2;
-                break;
-            case '134':
+           case '124':
+              elements.B1.value = b2 * a1 / a2;
+               break;
+           case '134':
                 elements.A2.value = a1 * b2 / b1;
-                break;
+               break;
             case '234':
                 elements.A1.value = a2 * b1 / b2;
                 break;
@@ -41,11 +46,13 @@ calculate = function (a1, a2, b1, b2) {
 
 initCalculation = function() {
     var clear = document.getElementById('delete');
+
     elements = {
         A1: document.getElementById('type-a-top'),
         A2: document.getElementById('type-a-bottom'),
         B1: document.getElementById('type-b-top'),
-        B2: document.getElementById('type-b-bottom')
+        B2: document.getElementById('type-b-bottom'),
+        B3: document.getElementById('type-c-bottom')
     };
 
     clear.onclick = function () {
@@ -53,6 +60,7 @@ initCalculation = function() {
         elements.A2.value = '';
         elements.B1.value = '';
         elements.B2.value = '';
+        elements.B3.value = '';
         lastOne = LAST_ONE_EMPTY;
     };
 
@@ -61,12 +69,16 @@ initCalculation = function() {
             elements.A1.value,
             elements.A2.value,
             elements.B1.value,
-            elements.B2.value
+            elements.B2.value,
+            elements.B3.value
+
         );
     };
 };
 
 // Start the functions.
-window.onload = function() {
+window.onload = function () {
+   
+
     initCalculation();
 };
